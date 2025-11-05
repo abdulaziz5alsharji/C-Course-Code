009bc517b646d9079993eb7ae4eeeb2a0c998e34
Abdulaziz Al-sharji <66184929+abdulaziz5alsharji@users.noreply.github.com>
2025-11-05 12:33:13 +0400

    Update first_lesson.cpp

--- first_lesson.cpp
+++ first_lesson.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <C:\Users\MOBPC\Desktop\Programming\C++\second_lesson.cpp>
 
 namespace calc {
     int number = 5;
@@ -11,7 +12,9 @@ namespace calc {
 using namespace calc;
 
 int main() {
-    std::cout << sum(3, 3) << "\n";
+    print();
+    print("Welomce to my App");
+    std::cout << sum(3, 3) << std::endl;
     std::cout<<"\nHello World, " << mess;
     return 0;
 }
